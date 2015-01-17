8214f5d26acc2e381b56c178559009b2961fc324
henrik.sorensen <henrik.sorensen@gmail.com>
2015-01-17 05:07:32 +0000

    util functions

--- Pathfinder/util.py
+++ Pathfinder/util.py
@@ -10,11 +10,11 @@ def subStringMatchItemInList(l, key, subString):
         return None
 
 def subStringMatchDictKey(d, subString):
-    f = lambda k : k.lower().find(subString.lower()) > -1
-    m = filter(f, d.keys())
-        
-    if m != []:
-        return (m[0], d[m[0]])
+    f = lambda key : key.lower().find(subString.lower()) > -1
+    foundKey = find(f, d)
+
+    if foundKey is not None:
+        return (foundKey, d[foundKey])
     return None
 
 def find(f, seq):
@@ -23,3 +23,41 @@ def find(f, seq):
         if f(item): 
             return item
 
+    return None
+
+def tryToConvertValue(value, noneValues = []):
+    if value is None or value in noneValues:
+        return None
+    try:
+        value = float(value)
+    except (ValueError, TypeError) as e:
+        pass
+
+    try:
+        value = int(value)
+    except (ValueError, TypeError) as e:
+        pass
+
+    return value
+
+def getGoldValue(pp, gp, sp, cp):
+    return (pp * 10) + gp + sp / 10.0 + cp / 100.0
+
+truthValues = ["yes", "true", "1", "t"]
+falseValues = ["no", "false", "0", "f"]
+
+def isStrBoolTrue(s, additionalTruthValues = []):
+    return s.lower() in truthValues + additionalTruthValues
+
+def isStrBool(s):
+    s = s.lower()
+    return s in truthValues or s in falseValues
+
+class DictEncapsulator(object):
+    def __init__(self, dict = {}):
+        self.__assignFromDict(dict)
+
+    def __assignFromDict(self, dict):
+        for k, v in dict.items():
+            if type(k) is str:
+                self.__setattr__(k, v)
